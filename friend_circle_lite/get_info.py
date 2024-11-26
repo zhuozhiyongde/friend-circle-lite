49ac3e2d980f42c40f370988ac2d2ce8d1de0222
CWorld <cworld0@qq.com>
2024-11-26 14:46:32 +0800

    fix: :bug: fix feed_url var name

--- friend_circle_lite/get_info.py
+++ friend_circle_lite/get_info.py
@@ -87,7 +87,7 @@ def check_feed(friend, session):
     for feed_type, path in possible_feeds:
         feed_url = rsslink if rsslink else blog_url + path
         try:
-            response = session.get(url, headers=headers, timeout=timeout)
+            response = session.get(feed_url, headers=headers, timeout=timeout)
             if response.status_code == 200:
                 return [url.split('/')[-1].split('.')[0], url]
         except requests.RequestException:
